00cdda9b500bb7720498ddc5a8b11442e619e500
DoanGiaHuy0301 <huydg0301@gmail.com>
2024-05-12 00:50:17 +0700

    update code

--- src/pages/student/payment/index.vue
+++ src/pages/student/payment/index.vue
@@ -1,16 +1,6 @@
 <template>
   <section>
     <div class="container">
-      <div class="container-spinner" v-if="isLoading">
-        <div class="spinner">
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-        </div>
-      </div>
       <select
         class="form-control form-width"
         id="selectSemester"
@@ -28,7 +18,7 @@
         </option>
       </select>
       <div v-if="isTuitionFeeSuccess" class="table-container">
-        <table class="table" style="width: 100%">
+        <table class="table" style="width: 100%; ">
           <thead>
             <tr class="table-title">
               <th scope="col" class="text-center" style="width: 5%">STT</th>
@@ -160,8 +150,6 @@ export default {
       listTuitionFee: [],
       listSubjectTuitionFee: [],
       semesters: [],
-      errorMessage: "",
-      isLoading: false,
     };
   },
   watch: {
@@ -202,24 +190,18 @@ export default {
       }
     },
     async submitTuitionFee(tuitionFeeId) {
-      try {
-        this.isLoading = true;
-        const res = await Apis.post(
-          endpoints["payment"] + `?tuitionFeeId=${tuitionFeeId}`
-        );
+      const res = await Apis.post(
+        endpoints["payment"] + `?tuitionFeeId=${tuitionFeeId}`
+      );
 
-        if (res.data) {
-          // Lấy token từ phản hồi
-          const paymentToken = res.data;
+      if (res.data) {
+        // Lấy token từ phản hồi
+        const paymentToken = res.data;
 
-          // Chuyển hướng người dùng đến trang PayPal
-          window.location.href = paymentToken;
-        } else {
-          console.error("Invalid payment token received.");
-        }
-        this.isLoading = false;
-      } catch (e) {
-        (this.errorMessage = "lỗi server"), console.log(e.error);
+        // Chuyển hướng người dùng đến trang PayPal
+        window.location.href = paymentToken;
+      } else {
+        console.error("Invalid payment token received.");
       }
     },
     async getListSemester() {
@@ -332,78 +314,6 @@ export default {
 .button:active:before,
 .button:active:after {
   background: teal;
-}
-
-.container-spinner {
-  position: fixed;
-  top: 0;
-  right: 0;
-  left: 0;
-  bottom: 0;
-  background: rgba(255, 255, 255, 0.5);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 2;
-}
-
-.spinner {
-  width: 70.4px;
-  height: 70.4px;
-  --clr: rgb(7, 7, 88);
-  --clr-alpha: rgb(7, 7, 88, 0.1);
-  animation: spinner 1.6s infinite ease;
-  transform-style: preserve-3d;
-}
-
-.spinner > div {
-  background-color: var(--clr-alpha);
-  height: 100%;
-  position: absolute;
-  width: 100%;
-  border: 3.5px solid var(--clr);
-}
-
-.spinner div:nth-of-type(1) {
-  transform: translateZ(-35.2px) rotateY(180deg);
-}
-
-.spinner div:nth-of-type(2) {
-  transform: rotateY(-270deg) translateX(50%);
-  transform-origin: top right;
-}
-
-.spinner div:nth-of-type(3) {
-  transform: rotateY(270deg) translateX(-50%);
-  transform-origin: center left;
-}
-
-.spinner div:nth-of-type(4) {
-  transform: rotateX(90deg) translateY(-50%);
-  transform-origin: top center;
-}
-
-.spinner div:nth-of-type(5) {
-  transform: rotateX(-90deg) translateY(50%);
-  transform-origin: bottom center;
-}
-
-.spinner div:nth-of-type(6) {
-  transform: translateZ(35.2px);
-}
-
-@keyframes spinner {
-  0% {
-    transform: rotate(45deg) rotateX(-25deg) rotateY(25deg);
-  }
-
-  50% {
-    transform: rotate(45deg) rotateX(-385deg) rotateY(25deg);
-  }
-
-  100% {
-    transform: rotate(45deg) rotateX(-385deg) rotateY(385deg);
-  }
 }
 
 @media (max-width: 768px) {
