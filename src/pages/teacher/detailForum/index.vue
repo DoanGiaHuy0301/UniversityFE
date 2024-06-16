9a2b062cb3f58bbf598b6cf263353eeb837002ea
DoanGiaHuy0301 <huydg0301@gmail.com>
2024-06-17 00:47:56 +0700

    update

--- src/pages/teacher/detailForum/index.vue
+++ src/pages/teacher/detailForum/index.vue
@@ -131,7 +131,7 @@ export default {
       editedPost: null,
       isLoading: false,
       isBadWordsComment: false,
-      badWordsList: "tệ",
+      badWordsList: "tệdở",
     };
   },
   created() {
